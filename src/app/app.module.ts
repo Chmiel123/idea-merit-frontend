2e6b4cbb2843d386766ce4deb3a342c53a5eca3b
Jakub Chmiel <chmiel123@gmail.com>
2020-11-21 16:55:00 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/model/account.model.ts
@@ -0,0 +1,7 @@
+import { v4 as uuid } from 'uuid';
+
+export class User {
+    id: string;
+    name: string;
+    car: Car;
+  }
